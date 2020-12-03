3c70f53172419b87b0e29cb6477796b5ebe72870
gmgn120 <gmgn120@gmail.com>
2020-12-03 03:55:43 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,8 +17,10 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(book.id)
+    end
   end
 
   def edit
@@ -27,19 +29,23 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+       flash[:notice] = "successfully"
+       redirect_to book_path(book)
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+       flash[:notice] = "successfully"
+       redirect_to books_path
+    end
   end
   
   private
   
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 end
